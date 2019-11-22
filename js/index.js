95c84abb923502e4da9d5e83764fa130a3e353c6
NastyaPronchuk <girloneshot@icloud.com>
2019-11-22 14:57:18 +0000

    Simple site

--- js/index.js
+++ js/index.js
@@ -4,3 +4,25 @@ const productList = new ProductList(
   $('.products-container'),
   cart
 );
+$(document).ready(function(){
+  $('body').append('<a href="#" id="go-top" title="Вверх">Вверх</a>');
+});
+
+$(function() {
+ $.fn.scrollToTop = function() {
+  $(this).hide().removeAttr("href");
+  if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+  var scrollDiv = $(this);
+  $(window).scroll(function() {
+   if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+   else $(scrollDiv).fadeIn("slow")
+  });
+  $(this).click(function() {
+   $("html, body").animate({scrollTop: 0}, "slow")
+  })
+ }
+});
+
+$(function() {
+ $("#go-top").scrollToTop();
+});
